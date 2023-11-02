05e96f3c8efd183afb6a62be5ab157042a715e36
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2023-11-02 11:20:00 -0400

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const forms = document.querySelectorAll(".side-form");
-    M.Sidenav.init(forms, { edge:  "left" });
-})
+const movies = document.querySelector(".movies");
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Add Movies
+  const forms = document.querySelectorAll(".side-form");
+  M.Sidenav.init(forms, { edge: "left" });
+});
+
+const renderMovie = (data, id) => {
+  const html = `
+  <div class="card-panel movie purple lighten-4 row" data-id ="${id}">
+            <img src="/public/img/clapboard.png" class="responsive-img materialboxed" alt="">
+            <div class="movie-detail">
+                <div class="movie-title">${data.title}</div>
+                <div class="movie-synopsis">${data.synopsis}</div>
+            </div>
+            <div class="movie-delete">
+                <i class="material-icons" data-id ="${id}">delete_outline</i>
+            </div>
+        </div>
+  `;
+
+  movies.innerHTML += html;
+};
+
+//remove movie from DOM
+const removeMovie = (id) => {
+  const movie = document.querySelector(`.movie[data-id =${id}]`);
+  movie.remove();
+};
