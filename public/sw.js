4d94068f917b88bdc8c1bc73fc0e6da7df80489b
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2023-11-19 12:45:25 -0500

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,67 @@
+const staticCache = "Static-cache-v5";
+const dynamicCache = "Dynamic-cache-v7";
+
+const assets = [
+  "/",
+  "/public/index.html",
+  "/public/fallback.html",
+  "/public/js/app.js",
+  "/public/js/ui.js",
+  "/public/js/materialize.min.js",
+  "/public/css/materialize.min.css",
+  "/public/css/app.css",
+  "/public/img/clapboard.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+];
+
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+self.addEventListener("install", function (event) {
+  console.log(`SW: Event fired: ${event.type}`);
+  event.waitUntil(
+    caches.open(staticCache).then(function (cache) {
+      console.log("SW: Precaching App shell");
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCache && key !== dynamicCache)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+//comment
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((fetchRes) => {
+            return caches.open(dynamicCache).then((cache) => {
+              cache.put(event.request.url, fetchRes.clone());
+              limitCacheSize(dynamicCache, 3);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => caches.match("/public/fallback.html"))
+  );
+});
