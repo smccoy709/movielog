e9cfe053df00a4bb4ee73c74487066860ffdef31
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2023-11-29 15:19:39 -0500

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     // Register our service worker
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("../sw.js").then((reg) => {
+        navigator.serviceWorker.register("./sw.js").then((reg) => {
             // Display a success message
             console.log(`Service Worker Registration (SCOPE:  ${reg.scope})`);
         }).catch((error) => {
